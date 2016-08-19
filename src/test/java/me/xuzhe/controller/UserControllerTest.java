06b023d78b3c728fa2df7bbf3ab5f4ac4cd76620
xuzhepku <xuzhepku@gmail.com>
2016-08-19 15:22:42 +0800

    junit 初步引入，没有深究

---
+++ src/test/java/me/xuzhe/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package me.xuzhe.controller;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by XuZhe on 2016/8/19.
+ */
+public class UserControllerTest extends TestCase {
+    @Test
+    public void test01(){
+        System.out.println("testtesttest");
+    }
+}
