a9bded4cfa8a2e993019539242ad949bf4dfe22c
xuzhepku <xuzhepku@gmail.com>
2016-08-20 09:20:37 +0800

    hibernate可以跑了（junit）

--- src/test/java/me/xuzhe/controller/UserControllerTest.java
+++ src/test/java/me/xuzhe/controller/UserControllerTest.java
@@ -1,14 +1,40 @@
 package me.xuzhe.controller;
 
-import junit.framework.TestCase;
+import me.xuzhe.model.HibernateUser;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.Test;
+
+import java.util.Date;
 
 /**
  * Created by XuZhe on 2016/8/19.
  */
-public class UserControllerTest extends TestCase {
+public class UserControllerTest {
     @Test
-    public void test01(){
+    public void test01() {
         System.out.println("testtesttest");
+        /* hibernate 5中不可用
+        Configuration cfg = new Configuration().configure();
+        ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+        SessionFactory factory = cfg.buildSessionFactory(sr);
+        */
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+        // 创建session
+        Session session = sf.openSession();
+        // 开启事务
+        session.beginTransaction();
+
+        HibernateUser hu = new HibernateUser();
+        hu.setUsername("username");
+        hu.setPassword("123");
+        hu.setNickname("nickname");
+        hu.setBornDate(new Date());
+        session.save(hu);
+        // 提交事务
+        session.getTransaction().commit();
+
     }
 }
